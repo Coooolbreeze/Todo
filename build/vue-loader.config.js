d1f8c2e645ee78079befe0ba26aad43bb4372f64
Coooolbreeze <392113643@qq.com>
2018-04-14 23:38:58 +0800

    配置生产环境下vue文件style标签内样式提取功能

--- build/vue-loader.config.js
+++ build/vue-loader.config.js
@@ -1,9 +1,19 @@
 module.exports = (isDev) => {
-  return {
+  let options = {
     preserveWhitepace: true,
     cssModules: {
       localIdentName: isDev ? '[path]-[name]-[hash:base64:5]' : '[hash:base64:5]',
       camelCase: true
     }
   }
+
+  if (!isDev) {
+    const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+    options.loaders = {
+      stylus: MiniCssExtractPlugin.loader + '!css-loader!postcss-loader!stylus-loader'
+    }
+  }
+
+  return options
 }
