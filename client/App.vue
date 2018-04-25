206de4afcfdd203ef26dba15ca0011a8d0d794c7
Coooolbreeze <392113643@qq.com>
2018-04-25 22:36:21 +0800

    app.vue改为App.vue

---
+++ client/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div :id="$style.app">
+    <div :id="$style.cover"></div>
+    <the-header />
+    <transition
+      name="fade"
+      mode="out-in"
+      :enter-active-class="$style['fade-enter-active']"
+      :leave-active-class="$style['fade-leave-active']"
+      :enter-class="$style['fade-enter']"
+      :leave-to-class="$style['fade-leave-to']"
+    >
+      <router-view />
+    </transition>
+    <the-footer />
+  </div>
+</template>
+
+<script>
+import TheHeader from './components/TheHeader.vue'
+import TheTodo from './components/TheTodo.vue'
+import TheFooter from './components/TheFooter.jsx'
+
+export default {
+  metaInfo: {
+    title: "Cooolbreeze's Todo App"
+  },
+  components: {
+    TheHeader,
+    TheTodo,
+    TheFooter
+  }
+}
+</script>
+
+<style lang="stylus" module>
+#app {
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+}
+
+#cover {
+  position: absolute;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #999999;
+  opacity: 0.8;
+  z-index: -1;
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.5s;
+}
+
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+</style>
